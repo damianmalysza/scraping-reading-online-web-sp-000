0052dc584ee17c837a6855e4c2a4b8cd5dad4494
Damian Malysza <damian.malysza@gmail.com>
2020-11-05 23:58:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,4 @@ require 'open-uri'
 require 'pry'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN")
+doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .title-oE5vT4")
